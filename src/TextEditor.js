40a4d5dbd4e94faf01db86c4f9317d78ee18b1cb
BADU\badus <badushaebrahim62@gmail.com>
2022-06-16 15:39:41 +0530

    submodule for ease  dev

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -23,7 +23,7 @@ export default function TextEditor() {
   const [quill, setQuill] = useState()
 
   useEffect(() => {
-    const s = io("http://localhost:3001")
+    const s = io("http://docback.eu-4.evennode.com:3001")
     setSocket(s)
 
     return () => {
